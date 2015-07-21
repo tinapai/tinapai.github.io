f012304f9a1e2cc91f909bbd5f3c2f1b25fd846a
tinapai <tinapai@berkeley.edu>
2015-07-21 15:18:00 -0700

    made tabs more flexible code

--- js/tabbing.js
+++ js/tabbing.js
@@ -1,10 +1,29 @@
 // tabbing stuff
-// from http://www.webhostingtalk.com/showthread.php?t=1045871
-function tab(tab) {
-document.getElementById(‘tab1′).style.display = ‘none’;
-document.getElementById(‘tab2′).style.display = ‘none’;
-document.getElementById(‘li_tab1′).setAttribute(“class”, “”);
-document.getElementById(‘li_tab2′).setAttribute(“class”, “”);
-document.getElementById(tab).style.display = ‘block’;
-document.getElementById(‘li_’+tab).setAttribute(“class”, “active”);
-}
+$(document).ready(function() {
+
+	var availableTabs = ['#tab1', '#tab2'];
+
+	function tab(tab) {
+		if (!$.inArray(tab, availableTabs)) {
+			console.log("invalid tab. Please update your available tabs");
+		} else {
+			var arrayLength = availableTabs.length;
+			for (var i = 0; i < arrayLength; i++) {
+				//maybe have problems where tab doesn't exist
+			    $(availableTabs[i] + 'Content').hide();
+			    $(availableTabs[i] + 'Content').css('class', '');
+			}
+			$("#" + tab + 'Content').show();
+			$("#" + tab + 'Content').css('class', 'active');;
+
+		}
+	};
+
+	$('#tab1').click(function() {
+		tab("tab1");
+	});
+
+	$('#tab2').click(function() {
+		tab("tab2");
+	});
+});
