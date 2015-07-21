a9f8c55043826c941b4de891c94183c18c9fc793
tinapai <tinapai@berkeley.edu>
2015-07-21 09:33:40 -0700

    added jquery, masonry, test stuff

---
+++ js/imageloader.js
@@ -0,0 +1,21 @@
+$("#demo").innerHTML = "My First JavaScript";
+$(document).ready() {
+	$("#demo").innerHTML = "My First JavaScript";
+	// make sure to do this onLoad, for example jQuery's $()
+	//var illustrationArray = array('placeholder1', 'placeholder2');
+// 	alert("yo");
+// 	var foo = document.createElement("img");
+// 	foo.src = "images/illustrations/Lava.png";
+
+// 	$(new Image()).attr('src', '' + "images/illustrations/Lava.png").appendTo($('.grid-item'));
+// 	document.getElementById("fooHolder").innerHTML = foo.toString();
+	
+// 	//$(".grid-item").src.appendChild(foo);
+// 	$(".grid-item").innerHTML("foo");
+
+// 	$('.grid').masonry({
+// 		// options
+// 		itemSelector: '.grid-item',
+// 		columnWidth: 200
+// });
+}
