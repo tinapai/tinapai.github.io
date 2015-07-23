12ad845981a305803e3cef98ae2dea3aa4eba0df
tinapai <tinapai@berkeley.edu>
2015-07-23 09:12:43 -0700

    added pictures to all 3 tabs, centered layout

--- js/imageloader.js
+++ js/imageloader.js
@@ -1,24 +1,7 @@
 
 $(document).ready(function() {
-
-	var illustrationArray = [
-		'<img class="ill" src="images/illustrations/Lava.png"/>',
-		'<img class="ill" src="images/illustrations/icecreamtruck.jpg"/>',
-		'<img class="ill" src="images/illustrations/TinachaunPoint.png">',
-		'<img class="ill" src="images/illustrations/Christmas2013Dad.png"/>',
-		'<img class="ill" src="images/illustrations/img670.png"/>',
-		'<img class="ill" src="images/illustrations/img671.png"/>',
-		'<img class="ill" src="images/illustrations/img674.png"/>',
-		'<img class="ill" src="images/illustrations/img676.png"/>',
-		'<img class="ill" src="images/illustrations/img679.png"/>',
-		'<img class="ill" src="images/illustrations/img682.png"/>',
-		'<img class="ill" src="images/illustrations/img683.png"/>',
-		];
-
-	//put all the images into the html
-	for (i = 0; i < illustrationArray.length; i++) { 
-    	$('.grid').append('<div class="grid-item">' + illustrationArray[i] + '</div>');
-	}
+	//start by loading tab 1
+	tab('tab1'); //see tab() in tabbing.js
 
 	//layout Masonry after each image loads
 	$('.grid').imagesLoaded(function() {
@@ -29,3 +12,56 @@ $(document).ready(function() {
 		});
     });
 });
+
+//put all the images in ARRAY into the html
+function loadImages(array) {
+	for (var i = 0; i < array.length; i++) { 
+    	$('#' + activeTab + 'Grid').append('<div class="grid-item">' + array[i] + '</div>');
+	}
+}
+
+var illustrationArray = [
+	'<img class="ill" src="images/illustrations/Lava.png"/>',
+	'<img class="ill" src="images/illustrations/icecreamtruck.jpg"/>',
+	'<img class="ill" src="images/illustrations/TinachaunPoint.png">',
+	'<img class="ill" src="images/illustrations/Christmas2013Dad.png"/>',
+	'<img class="ill" src="images/illustrations/img670.png"/>',
+	'<img class="ill" src="images/illustrations/img671.png"/>',
+	'<img class="ill" src="images/illustrations/img674.png"/>',
+	'<img class="ill" src="images/illustrations/img676.png"/>',
+	'<img class="ill" src="images/illustrations/img679.png"/>',
+	'<img class="ill" src="images/illustrations/img682.png"/>',
+	'<img class="ill" src="images/illustrations/img683.png"/>',
+	];
+
+var graphicsArray = [
+	'<img class="gra" src="images/infographics/20150619.budget.tpai-02.png"/>',
+	'<img class="gra" src="images/infographics/administration.tpai.png"/>',
+	'<img class="gra" src="images/infographics/Berkeley.tpai.png">',
+	'<img class="gra" src="images/infographics/bitcoin.tpai0.png"/>',
+	'<img class="gra" src="images/infographics/budget.tpai.png"/>',
+	'<img class="gra" src="images/infographics/Calso.tpai.png"/>',
+	'<img class="gra" src="images/infographics/descriptions.tpai.png"/>',
+	'<img class="gra" src="images/infographics/lawsuit.tpai.png"/>',
+	'<img class="gra" src="images/infographics/PAC-color.tpai.png"/>',
+	];
+
+var photoArray = [
+	'<img class="pho" src="images/photos/IMG_7713.jpg"/>',
+	// '<img class="pho" src="images/photos/icecreamtruck.jpg"/>',
+	// '<img class="pho" src="images/photos/TinachaunPoint.png">',
+	// '<img class="pho" src="images/photos/Christmas2013Dad.png"/>',
+	// '<img class="pho" src="images/photos/img670.png"/>',
+	// '<img class="pho" src="images/photos/img671.png"/>',
+	// '<img class="pho" src="images/photos/img674.png"/>',
+	// '<img class="pho" src="images/photos/img676.png"/>',
+	// '<img class="pho" src="images/photos/img679.png"/>',
+	// '<img class="pho" src="images/photos/img682.png"/>',
+	// '<img class="pho" src="images/photos/img683.png"/>',
+	];
+
+var tabArrayMap = {
+    'tab1': illustrationArray,
+    'tab2': graphicsArray,
+    'tab3': photoArray
+};
--- js/tabbing.js
+++ js/tabbing.js
@@ -1,28 +1,44 @@
 // tabbing stuff
 $(document).ready(function() {
-
-	var availableTabs = ['#tab1', '#tab2', '#tab3'];
-
-	function tab(tab) {
-		if (!$.inArray(tab, availableTabs)) {
-			console.log("invalid tab. Please update your available tabs");
-		} else {
-			var arrayLength = availableTabs.length;
-			for (var i = 0; i < arrayLength; i++) {
-				//maybe have problems where tab doesn't exist
-			    $(availableTabs[i] + 'Content').hide();
-			    $(availableTabs[i] + 'Content').css({'class': ''});
-			    $(availableTabs[i]).css({'background': '#B4B6BD'});
-			}
-			$("#" + tab + 'Content').show();
-			$("#" + tab + 'Content').css({'class': 'active'});
-			$("#" + tab).css({'background': '#E8E3E3'});
-
-		}
-	};
-
+	//when you click a tab, this will make a call to switch tabs
 	$('.tab').click(function() {
 		target = $(this).attr('id')
 		tab(target);
 	});
 });
+
+var availableTabs = ['tab1', 'tab2', 'tab3']; //used to be: var availableTabs = ['#tab1', '#tab2', '#tab3'];
+var activeTab = 'tab1';
+var loadedTabs = [];
+
+//switch tabs (changing the visibility of pages) such that TAB is the new active tab
+//parameter TAB is type String; should look like 'tab1' or 'tab2' etc.
+function tab(tab) {
+	if ($.inArray(tab, availableTabs) == -1) {
+		console.log("invalid tab. Please update your available tabs");
+	} else {
+		var arrayLength = availableTabs.length;
+		for (var i = 0; i < arrayLength; i++) {
+			//maybe have problems where tab doesn't exist
+		    $("#"+availableTabs[i] + 'Content').hide(); //added #'s
+		    $("#"+availableTabs[i] + 'Content').css({'class': ''});
+		    $("#"+availableTabs[i]).css({'background': '#B4B6BD'});
+		}
+		$("#" + tab + 'Content').show();
+		$("#" + tab + 'Content').css({'class': 'active'});
+		activeTab = tab;
+		$("#" + tab).css({'background': '#E8E3E3'});
+		if ($.inArray(tab, loadedTabs) == -1) {
+			loadTab(activeTab);
+		}
+		
+	}
+};
+
+//Loads the images for the tab you want
+//parameter TAB is type String; should look like 'tab1' or 'tab2' etc.
+function loadTab(tab) {
+	var arrayToLoad = tabArrayMap[tab]; //see Array tabArrayMap in imageloader.js
+	loadImages(arrayToLoad);
+	loadedTabs.push(tab);
+}
