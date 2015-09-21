2234d69a1cfd8a0c0241c7cb1fa1eb42d073e181
tinapai <tinapai@berkeley.edu>
2015-09-21 15:19:09 -0700

    restored computer web browser responsiveness

--- js/mobileResponse.js
+++ js/mobileResponse.js
@@ -1,3 +1,34 @@
 $(document).ready(function() {
 	body.style.setProperty('opacity', .5);
 });
+
+
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+    #container {
+        width: 100%;
+        font-size: 24pt;
+        }
+    #footer {
+        width: 100%;
+        background: red;
+    }
+    #myBio {
+        width: 70%;
+        margin-left: 15%;
+        margin-right: auto;
+        text-align: center;
+    }
+    #portraitWrapper {
+        margin-left: 35%;
+        margin-right: auto; 
+    }
+}
+
+Tab
+@media only screen and (min-device-width : 320px) and (max-device-width : 480px) {
+	.tab {
+	font-size: 16pt;
+	width: 40%;
+	font-color: blue;
+	}
+}
